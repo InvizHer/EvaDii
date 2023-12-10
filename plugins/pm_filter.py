217c704f8e34d51aae46d1aabcfe3c516ec52042
InvizHer <112791490+InvizHer@users.noreply.github.com>
2023-12-10 21:23:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -696,7 +696,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"Reasult for -  {search}\n\nSelect your movie quality below and just click on the button 👇"
+        cap = f"Reasult for -  {search} 👇"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
